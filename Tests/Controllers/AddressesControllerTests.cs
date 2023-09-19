5131f7c5546a47fab28fcb7acba8cdc8ce0fe5b7
sebastian.siemianowski <rudph2@gmail.com>
2023-09-19 23:35:43 +0100

    Added new Tests

--- Tests/Controllers/AddressesControllerTests.cs
+++ Tests/Controllers/AddressesControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tmm.Controllers;
@@ -44,21 +45,21 @@ namespace Tests
 
             _testAddresses = new List<Address>
             {
-                new Address 
-                { 
-                    AddressLine1 = "123 Main St", 
-                    AddressLine2 = "Apt 4B", 
-                    County = "Test County", 
-                    Postcode = "12345", 
+                new Address
+                {
+                    AddressLine1 = "123 Main St",
+                    AddressLine2 = "Apt 4B",
+                    County = "Test County",
+                    Postcode = "12345",
                     Town = "TestTown",
                     CustomerId = customer.Id
                 },
-                new Address 
-                { 
-                    AddressLine1 = "456 Side St", 
+                new Address
+                {
+                    AddressLine1 = "456 Side St",
                     AddressLine2 = "Suite 7A",
-                    County = "Test County", 
-                    Postcode = "67890", 
+                    County = "Test County",
+                    Postcode = "67890",
                     Town = "TestTown",
                     CustomerId = customer.Id
                 }
@@ -74,8 +75,15 @@ namespace Tests
             var result = await _controller.GetAddressesForCustomer(1);
             var actionResult = Assert.IsType<ActionResult<IEnumerable<Address>>>(result);
             var addresses = Assert.IsType<List<Address>>(actionResult.Value);
-            
+
             Assert.Equal(_testAddresses.Count, addresses.Count);
+        }
+
+        [Fact]
+        public async Task GetAddressesForCustomer_ShouldReturnNotFoundForNonExistentCustomer()
+        {
+            var result = await _controller.GetAddressesForCustomer(999);
+            Assert.IsType<NotFoundObjectResult>(result.Result);
         }
 
         [Fact]
@@ -84,54 +92,140 @@ namespace Tests
             var result = await _controller.GetAddressForCustomer(1, 1);
             var actionResult = Assert.IsType<ActionResult<Address>>(result);
             var address = Assert.IsType<Address>(actionResult.Value);
-            
+
             Assert.Equal(_testAddresses[0].AddressLine1, address.AddressLine1);
+        }
+
+        [Fact]
+        public async Task GetAddressForCustomer_ShouldReturnNotFoundForNonExistentAddress()
+        {
+            var result = await _controller.GetAddressForCustomer(1, 999);
+            Assert.IsType<NotFoundObjectResult>(result.Result);
         }
 
         [Fact]
         public async Task AddAddressForCustomer_ShouldIncreaseAddressCount()
         {
-            var newAddress = new Address 
-            { 
-                AddressLine1 = "789 Second St", 
-                County = "Test County 2", 
-                Postcode = "11122", 
+            var newAddress = new Address
+            {
+                AddressLine1 = "789 Second St",
+                County = "Test County 2",
+                Postcode = "11122",
                 Town = "AnotherTown"
             };
-            
+
             await _controller.AddAddressForCustomer(1, newAddress);
-            
+
             var addresses = await _context.Addresses.ToListAsync();
             Assert.Equal(_testAddresses.Count + 1, addresses.Count);
         }
 
         [Fact]
+        public async Task AddAddressForCustomer_ShouldReturnBadRequestForNullAddress()
+        {
+            var result = await _controller.AddAddressForCustomer(1, null);
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("Address cannot be null.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task AddAddressForCustomer_ShouldReturnNotFoundForNonExistentCustomer()
+        {
+            var newAddress = new Address
+            {
+                AddressLine1 = "789 Second St",
+                County = "Test County 2",
+                Postcode = "11122",
+                Town = "AnotherTown"
+            };
+            var result = await _controller.AddAddressForCustomer(999, newAddress);
+            Assert.IsType<NotFoundObjectResult>(result.Result);
+        }
+
+        [Fact]
         public async Task UpdateAddressForCustomer_ShouldModifyAddressDetails()
         {
-            var updatedAddress = new Address 
+            var updatedAddress = new Address
             {
                 Id = 1,
                 CustomerId = 1,
                 AddressLine1 = "Updated Main St",
-                AddressLine2 = "Apt 4B", 
-                County = "Updated County", 
-                Postcode = "54321", 
+                AddressLine2 = "Apt 4B",
+                County = "Updated County",
+                Postcode = "54321",
                 Town = "UpdatedTown"
             };
-            
+
             await _controller.UpdateAddressForCustomer(1, 1, updatedAddress);
-            
+
             var address = await _context.Addresses.FindAsync(1);
             Assert.Equal("Updated Main St", address.AddressLine1);
+        }
+
+        [Fact]
+        public async Task UpdateAddressForCustomer_ShouldReturnBadRequestForMismatchedIds()
+        {
+            var updatedAddress = new Address
+            {
+                Id = 2,
+                CustomerId = 1,
+                AddressLine1 = "Updated Main St",
+            };
+            var result = await _controller.UpdateAddressForCustomer(1, 1, updatedAddress);
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Mismatched address or customer IDs.", badRequestResult.Value);
         }
 
         [Fact]
         public async Task DeleteAddressForCustomer_ShouldDecreaseAddressCount()
         {
             await _controller.DeleteAddressForCustomer(1, 1);
-            
+
             var addresses = await _context.Addresses.ToListAsync();
             Assert.Equal(_testAddresses.Count - 1, addresses.Count);
+        }
+
+        [Fact]
+        public async Task DeleteAddressForCustomer_ShouldReturnBadRequestWhenLastAddress()
+        {
+            await _controller.DeleteAddressForCustomer(1, 2);
+            var result = await _controller.DeleteAddressForCustomer(1, 1);
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("A customer must have at least one address.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task AddAddressForCustomer_WhenAddingMainAddress_ShouldUnsetExistingMainAddress()
+        {
+            var newMainAddress = new Address
+            {
+                AddressLine1 = "789 Main St",
+                IsMain = true,
+                Postcode = "YourPostcodeHere",
+                Town = "YourTownHere"
+            };
+
+
+            await _controller.AddAddressForCustomer(1, newMainAddress);
+
+            var addresses = await _context.Addresses.Where(a => a.CustomerId == 1).ToListAsync();
+            Assert.Single(addresses.Where(a => a.IsMain));
+        }
+
+        [Fact]
+        public async Task UpdateAddressForCustomer_WhenUpdatingToMainAddress_ShouldUnsetExistingMainAddress()
+        {
+            var updatedMainAddress = new Address
+            {
+                Id = 1,
+                CustomerId = 1,
+                AddressLine1 = "123 Main St",
+                IsMain = true
+            };
+            await _controller.UpdateAddressForCustomer(1, 1, updatedMainAddress);
+
+            var addresses = await _context.Addresses.Where(a => a.CustomerId == 1).ToListAsync();
+            Assert.Single(addresses.Where(a => a.IsMain));
         }
 
         public void Dispose()
